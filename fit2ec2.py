3810859583ba5e8f0cbdc0d26ec9494eed591b77
Odyssee <otremoulis@gmail.com>
2020-05-10 00:40:39 +0200

    Refactor code

--- fit2ec2.py
+++ fit2ec2.py
@@ -15,38 +15,58 @@ import pathlib
 
 
 class Compute:
+
+    file_extension = "pickle"
+    tmpdir ="./tmp"
+    sleep_time_after_creation = 20
+    pkgname = "fit2ec2"
+
     def __init__(self, keyname=None):
+
+        # Try to preload notebook name. It's an URL Request to Jupyter server
+        ipyparams.notebook_name
+        if ipyparams.notebook_name is not '':
+            self.nb_name = ipyparams.notebook_name
+        else:
+            raise ValueError('Notebook name not loaded fast enough. Please retry in a moment')
+
+        # Create temp dir
+        if os.path.exists(self.tmpdir):
+            raise ValueError("Please remove {} directory before starting".format(self.tmpdir))
+        else:
+            pathlib.Path(self.tmpdir).mkdir(parents=True, exist_ok=True)
+
+        self.pipreqsnb = "pipreqsnb . --savepath {}".format(os.path.join(self.tmpdir,"requirements.txt"))
+
+
+        if keyname is None:
+            self.keyname = "{}{}".format("ec2-keypair-",time.strftime("%Y%m%d-%H%M%S"))
+        else:
+            self.keyname = keyname
+        self.keyfile = os.path.join(self.tmpdir, "{}.pem".format(self.keyname))
+
         self.resource = boto3.resource('ec2')
         self.client = boto3.client('ec2')
-        self.keyname = keyname
-        if self.keyname is None:
-            self.keyname = 'ec2-keypair-'+time.strftime("%Y%m%d-%H%M%S")
 
-        self.tmpdir ="./temp"
-        pathlib.Path(self.tmpdir).mkdir(parents=True, exist_ok=True)
-
+        self.model_script = os.path.join(self.tmpdir,"model.py")
         self.filesto = set(["run.sh"])
         self.filesfrom = set([])
         self.instance = None
         self.sshclient = None
         self.RSAkey = None
 
+
     def create(self, imageId='ami-06ce3edf0cff21f07', instanceType='t2.micro'):
-        # Create key pair
-        print("Creating key pair [{}.pem]...".format(self.keyname))
-        try :
-            key_pair = self.client.create_key_pair(KeyName=self.keyname)
-            KeyPairOut = str(key_pair['KeyMaterial'])
-            #print("Key: {}".format(KeyPairOut))
-            f = open(self.tmpdir+"/"+self.keyname+'.pem','w')
-            f.write(KeyPairOut)
-            f.close()
-            print("Key pair created!")
-        except ex.ClientError:
-            print("Warning: Key Pair already exist")
+
+        # Generate requirements
+        os.system(self.pipreqsnb)
+        self.__addfile(os.path.join(self.tmpdir,"requirements.txt"))
+
+        # Create Key Pair
+        self.__create_key_pair()
 
         # Create EC2 instance
-        print("Creating EC2 instance with type [{}]".format(instanceType))
+        print("Launching EC2 instance. Type: {}".format(instanceType))
         instances = self.resource.create_instances(
             ImageId=imageId,
             MinCount=1,
@@ -55,15 +75,14 @@ class Compute:
             KeyName=self.keyname,
             #UserData=userdata
         )
+
         while True:
-            time.sleep(2)
+            time.sleep(self.sleep_time_after_creation)
             instances_filter = self.resource.instances.filter(InstanceIds = [instances[0].id])
             self.instance = next(iter(instances_filter))
 
-            print("Wait for EC2 instance to  be created...")
-
             if self.instance.public_ip_address != None:
-                print("EC2 instance created!")
+                print("EC2 instance created:")
                 print("\tId: {}".format(self.instance.id))
                 print("\tPublic IP address: {}".format(self.instance.public_ip_address))
                 break
@@ -84,25 +103,37 @@ class Compute:
         #    GroupId=sg_id)
 
         # Init SSH Client
-        self.RSAkey = paramiko.RSAKey.from_private_key_file(self.tmpdir+"/"+self.keyname+".pem")
+        self.RSAkey = paramiko.RSAKey.from_private_key_file(self.keyfile)
         self.sshclient = paramiko.SSHClient()
         self.sshclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        # Generate requirements
-        print("Generating project Python requirements")
-        nb_name = ipyparams.notebook_name
-        os.system("pipreqsnb . --savepath "+self.tmpdir+"/requirements.txt")
-        self.__addfile(self.tmpdir+"/"+"requirements.txt")
-        #print("requirements.txt generated!")
+
+    def __create_key_pair(self):
+        # Create key pair
+        print("Creating key pair [{}]".format(self.keyfile))
+        try :
+            key_pair = self.client.create_key_pair(KeyName=self.keyname)
+            private_key = str(key_pair['KeyMaterial'])
+            open(self.keyfile,'w').write(private_key)
+        except ex.ClientError:
+            print("Warning: Key Pair already exist")
+
 
     def __addfile(self, name, retrieve=False):
         self.filesto.add(name)
         if retrieve:
             self.filesfrom.add(name)
 
+    def __clean_ec2(self):
+        sftp = self.__get_sftp()
+        for f in self.filesto:
+            base=os.path.basename(f)
+            sftp.remove(base)
+        self.sshclient.close()
+
 
     def __remote_exec(self, commands):
-        self.sshclient.connect(hostname=self.instance.public_ip_address, username="ec2-user", pkey=self.RSAkey)
+        self.__sshconnect()
         for command in commands:
             print("Executing [{}]".format(command))
             stdin, stdout, stderr = self.sshclient.exec_command(command)
@@ -115,32 +146,32 @@ class Compute:
         print("Deleting remote key pair...")
         self.client.delete_key_pair(KeyName=self.keyname)
         #self.resource.instances.filter(InstanceIds = [instanceId]).terminate()
-        print("Terminate EC2 instance [{}]...".format(self.instance.id))
+        print("Terminating EC2 instance [{}]".format(self.instance.id))
         self.instance.terminate()
-        print("Delete local key pair file...")
-        os.remove(self.tmpdir+"/"+self.keyname+".pem")
-        print("Done!")
+        print("Deleting local key pair file")
+        os.remove(self.keyfile)
+        print("Done")
 
     def fit(self, model,X,y):
         try:
             fmodel = self.__dump(model, "model")
-            self.__addfile(self.tmpdir+"/"+fmodel, retrieve=True)
+            self.__addfile(fmodel, retrieve=True)
 
             fX = self.__dump(X, "X")
-            self.__addfile(self.tmpdir+"/"+fX)
+            self.__addfile(fX)
 
             fy = self.__dump(y, "y")
-            self.__addfile(self.tmpdir+"/"+fy)
+            self.__addfile(fy)
 
             imports_nb = self.__get_imports()
             fin = open("template.py", "rt")
-            fout = open(self.tmpdir+"/"+"model.py", "wt")
+            fout = open(self.model_script, "wt")
             for line in fin:
                 fout.write(line.replace("{IMPORTS}", imports_nb))
             fin.close()
             fout.close()
 
-            self.__addfile(self.tmpdir+"/"+"model.py")
+            self.__addfile(self.model_script)
 
             # Tranfering all files to ec2
             self.__transferto(self.filesto)
@@ -148,7 +179,7 @@ class Compute:
 #            print("Error while connecting. Maybe too soon? Wait a couple of seconds for the instance to turn on")
 #            print(e)
         except Exception as e:
-            print("Error while preparing files")
+            print("Error while preparing and transfering files")
             print(e)
             raise
 
@@ -159,14 +190,21 @@ class Compute:
             return self.__load("model")
         except (KeyboardInterrupt, SystemExit, Exception) as e:
             print(e)
-            print("Stopping remote processes. (run terminate to destroy the ec2 instance)")
+            print("Stopping remote processes. (run *terminate* to destroy the ec2 instance)")
             self.__remote_exec(["pgrep run.sh | xargs pkill -9 -P"])
             sys.exit(0)
+            
+        self.__clean_ec2()
 
+    def __sshconnect(self):
+        self.sshclient.connect(hostname=self.instance.public_ip_address, username="ec2-user", pkey=self.RSAkey)
+
+    def __get_sftp(self):
+        self.__sshconnect()
+        return self.sshclient.open_sftp()
 
     def __transferto(self, files):
-        self.sshclient.connect(hostname=self.instance.public_ip_address, username="ec2-user", pkey=self.RSAkey)
-        sftp = self.sshclient.open_sftp()
+        sftp = self.__get_sftp()
         for f in files:
             print("Transfering [{}] to ec2".format(f))
             base=os.path.basename(f)
@@ -174,24 +212,39 @@ class Compute:
         self.sshclient.close()
 
     def __transferfrom(self, files):
-        self.sshclient.connect(hostname=self.instance.public_ip_address, username="ec2-user", pkey=self.RSAkey)
-        sftp = self.sshclient.open_sftp()
+        sftp = self.__get_sftp()
         for f in files:
             print("Retrieving [{}] from ec2".format(f))
             base=os.path.basename(f)
             sftp.get(base, f)
         self.sshclient.close()
 
+    def __dump(self, obj,name):
+        #print("Saving {}".format(fname))
+        fname = self.__to_pickle_name(name)
+        with open(fname, 'wb') as f:
+            pickle.dump(obj, f)
+        return fname
+
+    def __load(self, name):
+        #print("Loading {}".format(fname))
+        fname = self.__to_pickle_name(name)
+        with open(fname, 'rb') as f:
+            return pickle.load(f)
+
+    def __to_pickle_name(self, name):
+        return "{}/{}.{}".format(self.tmpdir, name, self.file_extension);
+
     ### Code from pipreqsnb ######
     def __get_imports(self):
         imports = []
-        nb_file = ipyparams.notebook_name
+        nb_file = self.nb_name
         nb = json.load(open(nb_file, 'r'))
         for cell in nb['cells']:
             if cell['cell_type'] == 'code':
                 valid_lines = self.__clean_invalid_lines_from_list_of_lines(cell['source'])
                 source = ''.join(valid_lines)
-                if "fit2ec2" not in source:
+                if self.pkgname not in source:
                     imports += self.__get_import_string_from_source(source)
 
         return '\n'.join(imports)
@@ -214,16 +267,3 @@ class Compute:
                 imports.append(splitted[node.lineno - 1])
         return imports
     ############################
-
-    def __dump(self, obj,name):
-        fname = "{}.pickle".format(name)
-        print("Saving {}".format(fname))
-        with open(self.tmpdir+"/"+fname, 'wb') as f:
-            pickle.dump(obj, f)
-        return fname
-
-    def __load(self, name):
-        fname = "{}.pickle".format(name)
-        print("Loading {}".format(fname))
-        with open(self.tmpdir+"/"+fname, 'rb') as f:
-            return pickle.load(f)
